7dafaa3cb017fcf1e2095c81f9649ae5e9139879
Muryong-Jeon <muryongjeon@gmail.com>
2020-08-30 18:39:32 +0900

    first commit

---
+++ Order/src/main/java/cnaShop/OrderRepository.java
@@ -0,0 +1,8 @@
+package cnaShop;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+public interface OrderRepository extends PagingAndSortingRepository<Order, Long>{
+
+
+}
